caf10fa6336b68ee80689c360e4cd69b291a9b09
vlrdimir <ronaldomoirangthem@gmail.com>
2025-11-09 23:09:01 +0700

    feat: add date range utility function for improved date handling
    - Introduced a new utility function `getDateRange` to calculate various date ranges such as today, this month, last month, and more.
    - Utilized `date-fns` for date formatting and manipulation, enhancing the application's date handling capabilities.
    - Improved the existing `cn` function with consistent formatting.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,57 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from "clsx";
+import { formatISO, startOfMonth, endOfMonth, subMonths } from "date-fns";
+import { twMerge } from "tailwind-merge";
+import type { DateRangeOption } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getDateRange = (dateRange: DateRangeOption) => {
+  const now = new Date();
+  switch (dateRange) {
+    case "today":
+      return {
+        start: formatISO(now, { representation: "date" }),
+        end: formatISO(now, { representation: "date" }),
+      };
+    case "this-month":
+      return {
+        start: formatISO(startOfMonth(now), { representation: "date" }),
+        end: formatISO(endOfMonth(now), { representation: "date" }),
+      };
+    case "last-month":
+      const lastMonth = subMonths(now, 1);
+      return {
+        start: formatISO(startOfMonth(lastMonth), { representation: "date" }),
+        end: formatISO(endOfMonth(lastMonth), { representation: "date" }),
+      };
+    case "last-2-months":
+      return {
+        start: formatISO(startOfMonth(subMonths(now, 2)), {
+          representation: "date",
+        }),
+        end: formatISO(endOfMonth(now), { representation: "date" }),
+      };
+    case "last-3-months":
+      return {
+        start: formatISO(startOfMonth(subMonths(now, 3)), {
+          representation: "date",
+        }),
+        end: formatISO(endOfMonth(now), { representation: "date" }),
+      };
+    case "this-year":
+      return {
+        start: formatISO(new Date(now.getFullYear(), 0, 1), {
+          representation: "date",
+        }),
+        end: formatISO(now, { representation: "date" }),
+      };
+    default:
+      // Default to this month
+      return {
+        start: formatISO(startOfMonth(now), { representation: "date" }),
+        end: formatISO(endOfMonth(now), { representation: "date" }),
+      };
+  }
+};
